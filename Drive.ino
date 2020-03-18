bb160af7fe7ba114cda76357a01c2d8c771ec575
FlavianOMEGA <flavianmelquiades@gmail.com>
2020-03-18 13:24:30 -0300

    Drive page now use Motor Page

--- Drive.ino
+++ Drive.ino
@@ -1,59 +1,53 @@
 void drive() {
-    readControll();
-    normallizer();
-    mix();
-    motorControll();
+  readControll();
+  normallizer();
+  mix();
+  motorControll();
 }
 
 void readControll() {
-    xAxy = analogRead(joystickXaxy);
-    yAxy = analogRead(joystickYaxy);
+  xAxy = analogRead(joystickXaxy);
+  yAxy = analogRead(joystickYaxy);
 
-    if (xAxy - 900 > -deadZone && xAxy - 900 < deadZone) {
-        xAxy = 441;
-    }
-    if (yAxy - 900 > -deadZone && yAxy - 900 < deadZone) {
-        yAxy = 441;
-    }
+  if (xAxy - 900 > -deadZone && xAxy - 900 < deadZone) {
+    xAxy = 441;
+  }
+  if (yAxy - 900 > -deadZone && yAxy - 900 < deadZone) {
+    yAxy = 441;
+  }
 }
 
 void normallizer() {
-    xValue = map(xAxy, 0, 900, -maxVelocity, maxVelocity);
-    yValue = map(yAxy, 0, 900, maxVelocity, -maxVelocity);
+  xValue = map(xAxy, 0, 900, -maxVelocity, maxVelocity);
+  yValue = map(yAxy, 0, 900, maxVelocity, -maxVelocity);
 }
 
 void mix() {
-    lOutput = yValue + xValue;
-    lOutput = constrain(lOutput, -maxVelocity, maxVelocity);
+  lOutput = yValue + xValue;
+  lOutput = constrain(lOutput, -maxVelocity, maxVelocity);
 
-    rOutput = yValue - xValue;
-    rOutput = constrain(rOutput, -maxVelocity, maxVelocity);
+  rOutput = yValue - xValue;
+  rOutput = constrain(rOutput, -maxVelocity, maxVelocity);
 }
 
 void motorControll() {
-    if (lOutput > 0) {
-        analogWrite(lPWM_R, 0);
-        analogWrite(lPWM_L, lOutput);
-    } else if (lOutput < 0) {
-        lOutput *= -1;
-        analogWrite(lPWM_R, lOutput);
-        analogWrite(lPWM_L, 0);
-        lOutput *= -1;
-    } else {
-        analogWrite(lPWM_R, 0);
-        analogWrite(lPWM_L, 0);
-    }
+  if (lOutput > 0) {
+    RodaEsquerdaFrente(lOutput);
+  } else if (lOutput < 0) {
+    lOutput *= -1;
+    RodaEsquerdaTras(lOutput);
+    lOutput *= -1;
+  } else {
+    RodaEsquerdaPara();
+  }
 
-    if (rOutput > 0) {
-        analogWrite(rPWM_R, 0);
-        analogWrite(rPWM_L, rOutput);
-    } else if (rOutput < 0) {
-        rOutput *= -1;
-        analogWrite(rPWM_R, rOutput);
-        analogWrite(rPWM_L, 0);
-        rOutput *= -1;
-    } else {
-        analogWrite(rPWM_R, 0);
-        analogWrite(rPWM_L, 0);
-    }
+  if (rOutput > 0) {
+    RodaDireitaFrente(rOutput);
+  } else if (rOutput < 0) {
+    rOutput *= -1;
+    RodaDireitaTras(rOutput);
+    rOutput *= -1;
+  } else {
+    RodaDireitaPara();
+  }
 }
