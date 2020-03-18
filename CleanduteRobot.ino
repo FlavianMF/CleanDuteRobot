1e800f812df8246ca91e2d00d89f96f92dafe2cd
FlavianOMEGA <flavianmelquiades@gmail.com>
2020-03-18 02:00:18 -0300

    Reorganizando o VS

--- CleanduteRobot.ino
+++ CleanduteRobot.ino
@@ -4,12 +4,13 @@
 #define maxVelocity 254
 #define maxBrushVelocity 254
 
-#define rPWM_R 5
-#define rPWM_L 6
-#define lPWM_R 9
-#define lPWM_L 10
+#define rPWM_R 3
+#define rPWM_L 5
+#define lPWM_R 6
+#define lPWM_L 9
 
-#define brushPin 3
+#define bPWM_L 10
+#define bPWM_R 11
 
 #define potentiometerPin A0
 
@@ -39,70 +40,74 @@ void RodaEsquerdaFrente(int PWM = pwm);
 void RodaEsquerdaTras(int PWM = pwm);
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    setupPins();
+  setupPins();
 
-    lcd.begin(16, 2);
-    lcd.setBacklight(HIGH);
+  lcd.begin(16, 2);
+  lcd.setBacklight(HIGH);
 }
 
 void loop() {
-    drive();
-    brush();
-    showInfos();
-    debug();
-    delay(10);
+  drive();
+  brush();
+  showInfos();
+  debug();
 }
 
 void setupPins() {
-    pinMode(rPWM_R, OUTPUT);
-    pinMode(rPWM_L, OUTPUT);
-    pinMode(lPWM_R, OUTPUT);
-    pinMode(lPWM_L, OUTPUT);
+  pinMode(rPWM_R, OUTPUT);
+  pinMode(rPWM_L, OUTPUT);
+  pinMode(lPWM_R, OUTPUT);
+  pinMode(lPWM_L, OUTPUT);
+  pinMode(bPWM_L, OUTPUT);
+  pinMode(bPWM_R, OUTPUT);
+  pinMode(potentiometerPin, INPUT);
+  pinMode(joystickXaxy, INPUT);
+  pinMode(joystickYaxy, INPUT);
 }
 void brush() {
-    getVelocity();
-    analogWrite(5, 0);
-    analogWrite(brushPin, velocity);
+  getVelocity();
+  analogWrite(bPWM_L, velocity);
+  analogWrite(bPWM_R, 0);
 }
 
 void getVelocity() {
-    rawPotentiometer = readPotentiometer();
-    if (rawPotentiometer > 900 || rawPotentiometer < 0) rawPotentiometer = 0;
-    velocity = normallizerPotentiometer(rawPotentiometer);
+  rawPotentiometer = readPotentiometer();
+  if (rawPotentiometer > 900 || rawPotentiometer < 0) rawPotentiometer = 0;
+  velocity = normallizerPotentiometer(rawPotentiometer);
 }
 
 uint16_t readPotentiometer() { return analogRead(potentiometerPin); }
 
 uint8_t normallizerPotentiometer(uint16_t potentiometerValue) {
-    return map(potentiometerValue, 0, 900, 0, maxBrushVelocity);
+  return map(potentiometerValue, 0, 900, 0, maxBrushVelocity);
 }
 
 void showInfos() {
-    lcd.clear();
-    lcd.setCursor(1, 0);
-    lcd.print("Brush Vel: ");
-    lcd.print(velocity);
-    lcd.setCursor(1, 1);
-    lcd.print("Robot Vel: ");
-    lcd.print(pwm);
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("Brush Vel: ");
+  lcd.print(velocity);
+  lcd.setCursor(1, 1);
+  lcd.print("Robot Vel: ");
+  lcd.print(pwm);
 }
 
 void debug() {
-    Serial.print("Raw Pot: ");
-    Serial.print(rawPotentiometer);
-    Serial.print("\tBrush Vel: ");
-    Serial.print(velocity);
-    Serial.print("\tRobot Vel: ");
-    Serial.print(pwm);
-    Serial.print("\tJoyX: ");
-    Serial.print(xAxy);
-    Serial.print("\tJoyY: ");
-    Serial.print(yAxy);
-    Serial.print("\tlOutput: ");
-    Serial.print(lOutput);
-    Serial.print("\trOutput: ");
-    Serial.print(rOutput);
-    Serial.println();
+  Serial.print("Raw Pot: ");
+  Serial.print(rawPotentiometer);
+  Serial.print("\tBrush Vel: ");
+  Serial.print(velocity);
+  Serial.print("\tRobot Vel: ");
+  Serial.print(pwm);
+  Serial.print("\tJoyX: ");
+  Serial.print(xAxy);
+  Serial.print("\tJoyY: ");
+  Serial.print(yAxy);
+  Serial.print("\tlOutput: ");
+  Serial.print(lOutput);
+  Serial.print("\trOutput: ");
+  Serial.print(rOutput);
+  Serial.println();
 }
